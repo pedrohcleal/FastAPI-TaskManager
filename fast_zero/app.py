a0c2057918cf65ceb86dd7f900750a51663f67ec
Pedro Leal <59542610+pedrohcleal@users.noreply.github.com>
2024-06-19 21:41:24 -0300

    aula 2

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,13 +1,31 @@
+from http import HTTPStatus
 from typing import Dict
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root() -> Dict[str, str]:
     """
     Apenas para testar o servidor
     """
     return {'message': 'hello word'}
+
+
+@app.get('/hello', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def hello() -> str:
+    """retornará hello world em html"""
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,11 +4,29 @@ from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
+client = TestClient(app)  # Arrange
+
 
 def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange
-
     response = client.get('/')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'hello word'}  # Assert
+
+
+def test_root_deve_retornar_hw_htm():
+    response = client.get('/hello')
+
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.text
+        == """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
+    )
