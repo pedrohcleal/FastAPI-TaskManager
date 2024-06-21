bad5a7bd18e59c7e18b51df0c14698ca5dfa6f2c
Pedro Leal <59542610+pedrohcleal@users.noreply.github.com>
2024-06-21 19:50:42 -0300

    first post and database on code

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -4,9 +4,12 @@ from typing import Dict
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
+from pprint import pprint
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -20,8 +23,7 @@ def read_root() -> Dict[str, str]:
 @app.get('/hello', status_code=HTTPStatus.OK, response_class=HTMLResponse)
 def hello() -> str:
     """retornará hello world em html"""
-    return """
-    <html>
+    return """<html>
       <head>
         <title> Nosso olá mundo!</title>
       </head>
@@ -29,3 +31,11 @@ def hello() -> str:
         <h1> Olá Mundo </h1>
       </body>
     </html>"""
+
+
+@app.post('/users', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+    pprint(database)
+    return user
--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -1,5 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class Message(BaseModel):
     message: str
+    email: EmailStr
+    password: str
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+class UserPublic(BaseModel):
+    username: str
+    email: EmailStr
+
+
+class UserDB(UserSchema):
+    id: int
