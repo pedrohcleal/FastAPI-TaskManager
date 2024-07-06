874dcc4ee1692361a8386282ea6a5ff25bd277f8
Pedro Leal <59542610+pedrohcleal@users.noreply.github.com>
2024-07-06 17:37:44 -0300

    Adicionada a primeira migração com Alembic. Criada tabela de usuários.

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -9,6 +9,7 @@ table_reg = registry()
 @table_reg.mapped_as_dataclass
 class User:
     __tablename__ = 'users'
+
     id: Mapped[int] = mapped_column(init=False, primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
--- migrations/env.py
+++ migrations/env.py
@@ -5,25 +5,16 @@ from sqlalchemy import pool
 
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
-config = context.config
+from fast_zero.models import table_reg
+from fast_zero.settings import Settings
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+config = context.config
+config.set_main_option('sqlalchemy.url', Settings().DATABASE_URL)
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+target_metadata = table_reg.metadata
 
 
 def run_migrations_offline() -> None:
